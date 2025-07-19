4acba0c435c4c48d693249f1e05cc55c8b12dde9
Mqhele-dot <89026892+Mqhele-dot@users.noreply.github.com>
2025-07-19 20:13:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-# Flask routing logic for Gaia dashboard
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
